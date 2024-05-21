a823d48db1d7add755b220e81eaec86e0dbf2b03
Isaiah <zazyzaya@gmail.com>
2024-05-21 10:55:31 -0400

    First commit

---
+++ evaluation.py
@@ -0,0 +1,389 @@
+import inspect
+import time
+from statistics import mean, stdev
+
+from tqdm import tqdm 
+from joblib import Parallel, delayed
+
+from CybORG import CybORG, CYBORG_VERSION
+from CybORG.Agents import SleepAgent, EnterpriseGreenAgent, FiniteStateRedAgent
+from CybORG.Simulator.Scenarios import EnterpriseScenarioGenerator
+
+from datetime import datetime
+
+import json
+
+import sys
+import os
+
+cyborg_version = CYBORG_VERSION
+EPISODE_LENGTH = 500
+
+def rmkdir(path: str):
+    """Recursive mkdir"""
+    partial_path = ""
+    for p in path.split("/"):
+        partial_path += p + "/"
+
+        if os.path.exists(partial_path):
+            if os.path.isdir(partial_path):
+                continue
+            if os.path.isfile(partial_path):
+                raise RuntimeError(f"Cannot create {partial_path} (exists as file).")
+
+        os.mkdir(partial_path)
+
+
+def load_submission(source: str):
+    """Load submission from a directory or zip file"""
+    sys.path.insert(0, source)
+
+    if source.endswith(".zip"):
+        try:
+            # Load submission from zip.
+            from submission.submission import Submission
+        except ImportError as e:
+            raise ImportError(
+                """
+                Error loading submission from zip.
+                Please ensure the zip contains the path submission/submission.py
+                """
+            ).with_traceback(e.__traceback__)
+    else:
+        # Load submission normally
+        from submission import Submission
+
+    # Remove submission from path.
+    sys.path.remove(source)
+    return Submission
+
+
+def evaluate_one_episode(cyborg, wrapped_cyborg, agent, write_to_file, i,tot):
+    observations, _ = wrapped_cyborg.reset()
+    r = []
+    a = []
+    o = []
+    count = 0
+    for j in tqdm(range(EPISODE_LENGTH), desc=f'({i+1}/{tot})'):
+        actions = {
+            agent_name: agent.get_action(
+                observations[agent_name], wrapped_cyborg.action_space(agent_name)
+            )
+            for agent_name, agent in submission.AGENTS.items()
+            if agent_name in wrapped_cyborg.agents
+        }
+        observations, rew, term, trunc, info = wrapped_cyborg.step(actions)
+        done = {
+            agent: term.get(agent, False) or trunc.get(agent, False)
+            for agent in wrapped_cyborg.agents
+        }
+        if all(done.values()):
+            break
+        r.append(mean(rew.values()))
+
+        if write_to_file:
+            a.append(
+                {
+                    agent_name: cyborg.get_last_action(agent_name)
+                    for agent_name in wrapped_cyborg.agents
+                }       
+            )
+            o.append(
+                {
+                    agent_name: observations[agent_name]
+                    for agent_name in observations.keys()
+                }
+            )
+    total_reward = sum(r)
+    return total_reward, a, o
+
+def run_evaluation_parallel(submission, log_path, max_eps=100, write_to_file=False, seed=None, workers=32):
+    cyborg_version = CYBORG_VERSION
+    EPISODE_LENGTH = 500
+    scenario = "Scenario4"
+
+    version_header = f"CybORG v{cyborg_version}, {scenario}"
+    author_header = f"Author: {submission.NAME}, Team: {submission.TEAM}, Technique: {submission.TECHNIQUE}"
+
+    envs = []
+    for _ in range(workers):
+        sg = EnterpriseScenarioGenerator(
+            blue_agent_class=SleepAgent,
+            green_agent_class=EnterpriseGreenAgent,
+            red_agent_class=FiniteStateRedAgent,
+            steps=EPISODE_LENGTH,
+        )
+        cyborg = CybORG(sg, "sim", seed=seed)
+        wrapped_cyborg = submission.wrap(cyborg)
+        envs.append((cyborg, wrapped_cyborg))
+    
+    print(version_header)
+    print(author_header)
+    print(
+        f"Using agents {submission.AGENTS}, if this is incorrect please update the code to load in your agent"
+    )
+
+    if write_to_file:
+        if not log_path.endswith("/"):
+            log_path += "/"
+        print(f"Results will be saved to {log_path}")
+
+    start = datetime.now()
+
+    outs = Parallel(prefer='processes', n_jobs=workers)(
+        delayed(evaluate_one_episode)(*envs[i % workers], submission.AGENTS, write_to_file, i, max_eps)
+        for i in range(max_eps)
+    )
+    total_reward, actions_log, obs_log = zip(*outs)
+
+    end = datetime.now()
+    difference = end - start
+
+    reward_mean = mean(total_reward)
+    reward_stdev = stdev(total_reward)
+    reward_string = (
+        f"Average reward is: {reward_mean} with a standard deviation of {reward_stdev}"
+    )
+    print(reward_string)
+
+    print(f"File took {difference} amount of time to finish evaluation")
+    if write_to_file:
+        print(f"Saving results to {log_path}")
+        with open(log_path + "summary.txt", "w") as data:
+            data.write(version_header + "\n")
+            data.write(author_header + "\n")
+            data.write(reward_string + "\n")
+            data.write(f"Using agents {submission.AGENTS}")
+
+        with open(log_path + "full.txt", "w") as data:
+            data.write(version_header + "\n")
+            data.write(author_header + "\n")
+            data.write(reward_string + "\n")
+            for act, obs, sum_rew in zip(actions_log, obs_log, total_reward):
+                data.write(
+                    f"actions: {act},\n observations: {obs},\n total reward: {sum_rew}\n"
+                )
+        
+        with open(log_path + "actions.txt", "w") as data:
+            data.write(version_header + "\n")
+            data.write(author_header + "\n")
+            data.write(reward_string + "\n")
+            for act in zip(actions_log):
+                data.write(
+                    f"actions: {act}"
+                )
+
+        with open(log_path + "summary.json", "w") as output:
+            data = {
+                "submission": {
+                    "author": submission.NAME,
+                    "team": submission.TEAM,
+                    "technique": submission.TECHNIQUE,
+                },
+                "parameters": {
+                    "seed": seed,
+                    "episode_length": EPISODE_LENGTH,
+                    "max_episodes": max_eps,
+                },
+                "time": {
+                    "start": str(start),
+                    "end": str(end),
+                    "elapsed": str(difference),
+                },
+                "reward": {
+                    "mean": reward_mean,
+                    "stdev": reward_stdev,
+                },
+                "agents": {
+                    agent: str(submission.AGENTS[agent]) for agent in submission.AGENTS
+                },
+            }
+            json.dump(data, output)
+
+        with open(log_path + "scores.txt", "w") as scores:
+            scores.write(f"reward_mean: {reward_mean}\n")
+            scores.write(f"reward_stdev: {reward_stdev}\n")
+
+def run_evaluation(submission, log_path, max_eps=100, write_to_file=False, seed=None):
+    cyborg_version = CYBORG_VERSION
+    EPISODE_LENGTH = 500
+    scenario = "Scenario4"
+
+    version_header = f"CybORG v{cyborg_version}, {scenario}"
+    author_header = f"Author: {submission.NAME}, Team: {submission.TEAM}, Technique: {submission.TECHNIQUE}"
+
+    sg = EnterpriseScenarioGenerator(
+        blue_agent_class=SleepAgent,
+        green_agent_class=EnterpriseGreenAgent,
+        red_agent_class=FiniteStateRedAgent,
+        steps=EPISODE_LENGTH,
+    )
+    cyborg = CybORG(sg, "sim", seed=seed)
+    wrapped_cyborg = submission.wrap(cyborg)
+    
+    print(version_header)
+    print(author_header)
+    print(
+        f"Using agents {submission.AGENTS}, if this is incorrect please update the code to load in your agent"
+    )
+
+    if write_to_file:
+        if not log_path.endswith("/"):
+            log_path += "/"
+        print(f"Results will be saved to {log_path}")
+
+    start = datetime.now()
+
+    total_reward = []
+    actions_log = []
+    obs_log = []
+    for i in tqdm(range(max_eps)):
+        observations, _ = wrapped_cyborg.reset()
+        r = []
+        a = []
+        o = []
+        count = 0
+        for j in range(EPISODE_LENGTH):
+            actions = {
+                agent_name: agent.get_action(
+                    observations[agent_name], wrapped_cyborg.action_space(agent_name)
+                )
+                for agent_name, agent in submission.AGENTS.items()
+                if agent_name in wrapped_cyborg.agents
+            }
+            observations, rew, term, trunc, info = wrapped_cyborg.step(actions)
+            done = {
+                agent: term.get(agent, False) or trunc.get(agent, False)
+                for agent in wrapped_cyborg.agents
+            }
+            if all(done.values()):
+                break
+            r.append(mean(rew.values()))
+            if write_to_file:
+                a.append(
+                    {
+                        agent_name: cyborg.get_last_action(agent_name)
+                        for agent_name in wrapped_cyborg.agents
+                    }       
+                )
+                o.append(
+                    {
+                        agent_name: observations[agent_name]
+                        for agent_name in observations.keys()
+                    }
+                )
+        total_reward.append(sum(r))
+
+        if write_to_file:
+            actions_log.append(a)
+            obs_log.append(o)
+
+    end = datetime.now()
+    difference = end - start
+
+    reward_mean = mean(total_reward)
+    reward_stdev = stdev(total_reward)
+    reward_string = (
+        f"Average reward is: {reward_mean} with a standard deviation of {reward_stdev}"
+    )
+    print(reward_string)
+
+    print(f"File took {difference} amount of time to finish evaluation")
+    if write_to_file:
+        print(f"Saving results to {log_path}")
+        with open(log_path + "summary.txt", "w") as data:
+            data.write(version_header + "\n")
+            data.write(author_header + "\n")
+            data.write(reward_string + "\n")
+            data.write(f"Using agents {submission.AGENTS}")
+
+        with open(log_path + "full.txt", "w") as data:
+            data.write(version_header + "\n")
+            data.write(author_header + "\n")
+            data.write(reward_string + "\n")
+            for act, obs, sum_rew in zip(actions_log, obs_log, total_reward):
+                data.write(
+                    f"actions: {act},\n observations: {obs},\n total reward: {sum_rew}\n"
+                )
+        
+        with open(log_path + "actions.txt", "w") as data:
+            data.write(version_header + "\n")
+            data.write(author_header + "\n")
+            data.write(reward_string + "\n")
+            for act in zip(actions_log):
+                data.write(
+                    f"actions: {act}"
+                )
+
+        with open(log_path + "summary.json", "w") as output:
+            data = {
+                "submission": {
+                    "author": submission.NAME,
+                    "team": submission.TEAM,
+                    "technique": submission.TECHNIQUE,
+                },
+                "parameters": {
+                    "seed": seed,
+                    "episode_length": EPISODE_LENGTH,
+                    "max_episodes": max_eps,
+                },
+                "time": {
+                    "start": str(start),
+                    "end": str(end),
+                    "elapsed": str(difference),
+                },
+                "reward": {
+                    "mean": reward_mean,
+                    "stdev": reward_stdev,
+                },
+                "agents": {
+                    agent: str(submission.AGENTS[agent]) for agent in submission.AGENTS
+                },
+            }
+            json.dump(data, output)
+
+        with open(log_path + "scores.txt", "w") as scores:
+            scores.write(f"reward_mean: {reward_mean}\n")
+            scores.write(f"reward_stdev: {reward_stdev}\n")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser("CybORG Evaluation Script")
+    parser.add_argument(
+        "--append-timestamp",
+        action="store_true",
+        help="Appends timestamp to output_path",
+    )
+    parser.add_argument(
+        "--seed", type=int, default=None, help="Set the seed for CybORG"
+    )
+
+    # Added to speed up evaluation 
+    parser.add_argument(
+        '--distribute', type=int, default=1, help="How many parallel workers to use"
+    )
+    parser.add_argument("--max-eps", type=int, default=100, help="Max episodes to run")
+    args = parser.parse_args()
+    args.output_path = os.path.abspath('tmp')
+    args.submission_path = os.path.abspath('')
+
+    if not args.output_path.endswith("/"):
+        args.output_path += "/"
+
+    if args.append_timestamp:
+        args.output_path += time.strftime("%Y%m%d_%H%M%S") + "/"
+
+    rmkdir(args.output_path)
+
+    submission = load_submission(args.submission_path)
+
+    if args.distribute == 1:
+        run_evaluation(
+            submission, max_eps=args.max_eps, log_path=args.output_path, seed=args.seed
+        )
+    else: 
+        run_evaluation_parallel(
+            submission, max_eps=args.max_eps, log_path=args.output_path, seed=args.seed, workers=args.distribute
+        )
